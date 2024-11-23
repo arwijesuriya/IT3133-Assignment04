0d9f9fab59a38cad338253b9ecb4ce659042756c
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-11-23 12:40:11 +0530

    imported db and displayed all the animals

--- assignment04/src/App.js
+++ assignment04/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Result from "./components/Result";
 import Name from "./components/Name";
 import Select from "./components/Select";
+
+// import database
+import { animals } from "./assets/AnimalsDb";
 
 function App() {
   return (
@@ -23,7 +27,7 @@ function App() {
         <Row>
           <Col xs={3}><div className='component-border'><Result/></div></Col>
           <Col xs={3}><div className='component-border'><Name/></div></Col>
-          <Col><div className='component-border'><Select/></div></Col>
+          <Col><div className='component-border'><Select animals={animals}/></div></Col>
         </Row>
       </Container>
     </div>
--- assignment04/src/components/Select.js
+++ assignment04/src/components/Select.js
@@ -1,7 +1,25 @@
-export default function Select() {
-    return(
-        <div>
-            <h2>Select the Animal</h2>
-        </div>
-    );
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+export default function Select({ animals }) {
+  return(
+    <Container>
+      <h2>Select the Animal</h2>
+      <Row className="gy-3">
+        {   
+            animals.map((animal) => (
+                <Col xs={6} md={3} className="text-center">
+                    <Button variant="light" className="p-2">
+                    <img
+                        src={require(`../assets/fig/${animal.img}`)}
+                        alt={animal.name}
+                        style={{ width: "100px", height: "100px", border:"black" }}
+                    />
+                    </Button>
+                </Col>
+            ))
+        }
+      </Row>
+    </Container>
+  );
 }
